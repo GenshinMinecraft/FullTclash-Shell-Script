4b1e35074f1549a5af7df440f448692437272449
GenshinMinecraft <68975944+GenshinMinecraft@users.noreply.github.com>
2023-04-27 12:39:48 +0800

    Update FTC.sh

--- FTC.sh
+++ FTC.sh
@@ -9,7 +9,7 @@
 # 运行本脚本前需要先运行 wget -O install_FTC.sh "https://raw.githubusercontents.com/GenshinMinecraft/FullTclash-Shell-Script/main/install.sh" && bash install_FTC.sh
 # 切勿单独运行
 
-red='\033[0;31m'
+export red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
@@ -29,39 +29,94 @@ echo -e "$red FullTclash 管理脚本$yellow
 7) 编辑配置文件（VIM）$plain
 "
 
+systemctl status FullTclash | grep -E 'running' >> /dev/null
+if [[ $? -ne 0 ]]; then
+    echo "当前状态：$red未启动！$plain"
+else
+    echo "当前状态：$green已启动！$plain"
+fi
+
 read -p "序号选择：" num
 case $num in
     0)
         wget -O /usr/bin/FTC "https://raw.githubusercontents.com/GenshinMinecraft/FullTclash-Shell-Script/main/FTC.sh"
         chmod +x /usr/bin/FTC
-        echo "$green已成功更新并自动退出！"
+        echo -e "$green已成功更新并自动退出！$plain"
         exit 0
         ;;
     1)
         systemctl daemon-reload
         systemctl start FullTclash
-        echo "$green已成功启动FullTclash！"
+        echo -e "$green已成功启动FullTclash！"
         exit 0
         ;;
     2)
         systemctl daemon-reload
         systemctl stop FullTclash
-        echo "$green已成功停止FullTclash！"
+        echo -e "$green已成功停止FullTclash！"
         exit 0
         ;;
     3)
         systemctl daemon-reload
         systemctl restart FullTclash
-        echo "$green已成功重启FullTclash！"
+        echo -e "$green已成功重启FullTclash！"
         exit 0
         ;;
     4)
         systemctl disable FullTclash
         rm -rf /usr/local/FullTclash/ /etc/systemd/system/multi-user.target.wants/FullTclash.service /etc/systemd/system/FullTclash.service
-        echo "$green已成功卸载FullTclash！"
-        echo "如需彻底卸载本脚本，请手动执行 rm -rf /usr/bin/FTC "
+        echo -e "$green已成功卸载FullTclash！"
+        echo -e "如需彻底卸载本脚本，请手动执行 rm -rf /usr/bin/FTC "
         exit 0
         ;;
+    5) 
+        systemctl enable FullTclash
+        echo -e "$green已成功设置开机启动FullTclash！"
+        exit 0
+        ;;
+    6) 
+        systemctl disable FullTclash
+        echo -e "$green已成功设置开机不启动FullTclash！"
+        exit 0
+        ;;
+    7) 
+        echo -e "$yellow注：您正在进行危险操作，请注意配置文件格式，编辑不当会导致启动错误！$plain"
+        sleep 1
+        YN=0
+        read -p "你确定吗(Y/N)：" YN 
+        
+        if [[ $YN == "y" || $YN == "Y" || $YN == "" ]]
+        then
+        echo -e "$yellow下载config.yaml.example中$plain" 
+        wget -O /usr/local/FullTclash/resources/config.yaml.example "https://raw.githubusercontents.com/AirportR/FullTclash/master/resources/config.yaml.example" 
+        apt install vim -y
+        
+        echo -e "$red在编辑完成后请保存并退出，将会自动重启与备份先前的config.yaml$plain"
+        sleep 3
+        vim /usr/local/FullTclash/resources/config.yaml.example
+
+        mv /usr/local/FullTclash/resources/config.yaml /usr/local/FullTclash/resources/config.yaml.bak
+        mv /usr/local/FullTclash/resources/config.yaml.example /usr/local/FullTclash/resources/config.yaml
+        echo -e "$yellow编辑完成，已备份先前config.yaml为config.yaml.bak。如当前config.yaml无法启动，将自动切换回config.yaml.bak$plain"
+
+        systemctl daemon-reload
+        systemctl restart FullTclash
+        echo -e "$green正在尝试使用新配置文件进行启动，请稍等15sec......$plain"
+        sleep 15
+
+        systemctl status FullTclash | grep -E 'running' >> /dev/null
+        if [[ $? -ne 0 ]]; then
+        echo -e "$red !!!错误！无法加载新配置文件，即将切换为旧配置!!!$plain"
+        mv /usr/local/FullTclash/resources/config.yaml.bak /usr/local/FullTclash/resources/config.yaml
+        systemctl restart FullTclash
+        echo -e "$green已切换为旧配置并启动$plain"
+        exit 0
+        fi
+    
+        
+        fi
+        ;;
     *)
-        echo "error"
+        echo -e "$red输入命令有误，请重试！$plain"
+        exit 1
 esac
